1c0699f828f22e3dd23555e6e9ad31e9781bb0c6
Alexandre Marques Bulcao <ambulcao@gmail.com>
2022-02-03 20:32:27 +0000

    Creating useState

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import React, { useState } from 'react'
 import './app.css'
 
 export default function App(){
+  const [peso, setPeso] = useState('');
+  const [altura, setAltura] = useState('');
+
+  function calcularIMC(){
+    const alt = altura / 100;
+    const imc = peso / (alt * alt);
+
+    if(imc < 18.6){
+      alert("Você está abaixo do peso! Seu IMC: " + imc.toFixed(2))
+    }else if(imc >= 18.6 && imc < 24.9){
+      alert("Peso ideal! Seu IMC: " + imc.toFixed(2))
+    }else if(imc >= 24.9 && imc < 34.9){
+      alert("Você está levemente acima do peso! Seu IMC: " + imc.toFixed(2))
+    }else if(imc > 34.9){
+      alert("Cuidado Obesidade! Seu IMC: " + imc.toFixed(2))
+    }
+  }
+
   return (
     <div className="app">
       <h1>Calculadora IMC</h1>
@@ -10,13 +29,17 @@ export default function App(){
         <input 
           type="text"
           placeholder="Peso em (kg) Ex: 90"
+          value={peso}
+          onChange={(e) => setPeso(e.target.value)}
         />
         <input 
           type="text"
           placeholder="Altura em (cm) Ex: 180"
+          value={altura}
+          onChange={(e) => setAltura(e.target.value)}
         />
 
-        <button>
+        <button onClick={calcularIMC}>
           Calcular
         </button>        
       </div>
