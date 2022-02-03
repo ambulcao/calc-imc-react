01ddbd49efb01a76d980eb9924fece511177578d
Alexandre Marques Bulcao <ambulcao@gmail.com>
2022-02-03 20:38:01 +0000

    Creating useState message

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ export default function App(){
   const [peso, setPeso] = useState('');
   const [altura, setAltura] = useState('');
 
+  const [mensagem, setMensagem] = useState('');
+
   function calcularIMC(){
     const alt = altura / 100;
     const imc = peso / (alt * alt);
 
     if(imc < 18.6){
-      alert("Você está abaixo do peso! Seu IMC: " + imc.toFixed(2))
+      setMensagem("Você está abaixo do peso! Seu IMC: " + imc.toFixed(2))
     }else if(imc >= 18.6 && imc < 24.9){
-      alert("Peso ideal! Seu IMC: " + imc.toFixed(2))
+      setMensagem("Peso ideal! Seu IMC: " + imc.toFixed(2))
     }else if(imc >= 24.9 && imc < 34.9){
-      alert("Você está levemente acima do peso! Seu IMC: " + imc.toFixed(2))
+      setMensagem("Você está levemente acima do peso! Seu IMC: " + imc.toFixed(2))
     }else if(imc > 34.9){
-      alert("Cuidado Obesidade! Seu IMC: " + imc.toFixed(2))
+      setMensagem("Cuidado Obesidade! Seu IMC: " + imc.toFixed(2))
     }
   }
 
@@ -43,7 +45,7 @@ export default function App(){
           Calcular
         </button>        
       </div>
-      <h2>Seu imc foi 25, você está abaixo do peso!</h2>
+      <h2>{mensagem}</h2>
     </div>
   )
 }
